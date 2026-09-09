00eeb2d4ee29346d57968f2da1659226ed07fb66
edward67-ccapa <edward.yllanes@ccapasistemas.com>
2026-09-09 16:30:29 -0500

    Fix Livewire asset_url by resetting to null

--- config/livewire.php
+++ config/livewire.php
@@ -116,7 +116,7 @@ return [
 
     'inject_assets' => true,
 
-    'asset_url' => env('ASSET_URL', env('APP_URL')),
+    'asset_url' => null,
 
     'update_uri' => env('LIVEWIRE_UPDATE_URI', null),
 
